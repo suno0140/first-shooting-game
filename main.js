97e8a0eea0c1a49b228c7d18e2aaca0f36518204
suno0140 <vlfxhd15@naver.com>
2023-04-18 14:18:03 +0900

    design: background, spaceship render 성공

--- main.js
+++ main.js
@@ -8,3 +8,40 @@ canvas.width = 400;
 canvas.height = 700;
 
 document.body.appendChild(canvas);
+
+let backgroundImage, spaceshipImage, bulletImage, enemyImage, gameOverImage;
+
+//우주선 좌표
+let spaceshipX = canvas.width / 2 - 30;
+let spaceshipY = canvas.height - 60;
+
+function loadImage() {
+  backgroundImage = new Image();
+  backgroundImage.src = "images/background.jpeg";
+
+  spaceshipImage = new Image();
+  spaceshipImage.src = "images/spaceship.png";
+
+  bulletImage = new Image();
+  bulletImage.src = "images/bullet.png";
+
+  enemyImage = new Image();
+  enemyImage.src = "images/enemy.png";
+
+  gameOverImage = new Image();
+  gameOverImage.src = "images/gameover.jpeg";
+}
+
+function render() {
+  ctx.drawImage(backgroundImage, 0, 0, canvas.width, canvas.height);
+  ctx.drawImage(spaceshipImage, spaceshipX, spaceshipY);
+}
+
+function main() {
+  render();
+  requestAnimationFrame(main);
+  console.log("animation calls main function");
+}
+
+loadImage();
+main();
