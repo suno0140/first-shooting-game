813e0fd34f4c18a342cde20f2707c997d8baace9
suno0140 <vlfxhd15@naver.com>
2023-04-18 13:17:19 +0900

    feat: canvas 세팅

---
+++ main.js
@@ -0,0 +1,10 @@
+//캔버스 세팅
+let canvas;
+let ctx;
+
+canvas = document.createElement("canvas");
+ctx = canvas.getContext("2d");
+canvas.width = 400;
+canvas.height = 700;
+
+document.body.appendChild(canvas);
